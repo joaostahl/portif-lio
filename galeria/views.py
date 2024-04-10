58e32d655f745614fbc405886c673c38da19150a
joaostahl <137836398+joaostahl@users.noreply.github.com>
2024-04-10 10:30:11 -0300

    index

--- galeria/views.py
+++ galeria/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse('<h1>Portifólio</h1><p>Bem Vindo a exposição do meu trabalho</p>') 
+    return render(request, 'index.html')
